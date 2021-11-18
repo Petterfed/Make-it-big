48b48582fa73d0d692d0b870c4cf48b89a992504
Petter <94312479+Petterfed@users.noreply.github.com>
2021-11-18 19:51:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = document.getElementById("text");
+
+const button = document.getElementById("button");
+
+
+button.addEventListener("click", function(){
+    text.value = text.value.toUpperCase();
+
+});
+
+
+
+
